e956ade47aa54791adfb07e826ca518a37ef6a63
yogimandhare <mandhare.yogendra12101998@gmail.com>
2025-08-20 15:01:12 +0530

    Added Header Component & Styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
