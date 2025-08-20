b5e4bb62e7d124ab944acd2387841781370166a3
yogimandhare <mandhare.yogendra12101998@gmail.com>
2025-08-21 00:07:16 +0530

    Added Dummy Users & Randomly Show on UI

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.css'
 })
 export class User {
-
+  selectedUser = DUMMY_USERS[randomIndex]; // Default to the first user
 }
